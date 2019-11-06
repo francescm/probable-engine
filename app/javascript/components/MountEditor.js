410cff9c577b4d288257931defea9af6e9d8c131
francesco <francesco.mlvz@gmail.com>
2019-11-06 18:49:16 +0100

    MountEditor rewritten with hooks

--- app/javascript/components/MountEditor.js
+++ app/javascript/components/MountEditor.js
@@ -1,48 +1,20 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React, { useState } from "react"
 import MountList from './MountList';
 import MountForm from './MountForm';
 
-class MountEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    //this.handleChange = this.handleChange.bind(this);
-    this.state = {mounts: this.props.mounts};
-  }
+const MountEditor = (params) => {
+  const { csrf, mymounts } = params;	
+  const [ mounts, setMounts ] = useState(mymounts);
 
-  handleMountSubmit = (data) => {
-	  const csrf = this.props.csrf;
-	  let mounts = this.state.mounts;
-	  let response = "";
-	  console.log(data);
-	  fetch('/mounts.json', {
-		  method: 'POST',
-		  headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-Token': csrf
-                  },		  
-		  body: JSON.stringify(data),
-	  }).then(response => response.json())
-	    .then(new_mount => { 
-	  	console.log(`mount is ${new_mount}`);
-    		mounts.push(new_mount);  
-    		this.setState({mounts: mounts});
-    		console.log(this.state);
-	    });
-  }
-
-  render () {
-    return (
+  return (
       <React.Fragment>
-	<MountList mounts={this.state.mounts} />
-	<MountForm onSubmit={this.handleMountSubmit}/>
+	<MountList mounts={mounts} />
+	<MountForm 
+	    onSubmit={setMounts}
+	    mounts={mounts}
+	    csrf={csrf}/>
       </React.Fragment>
     );
-  }
 }
 
-MountEditor.propTypes = {
-  mounts: PropTypes.array,
-  csrf: PropTypes.string
-};
 export default MountEditor
--- app/javascript/components/MountForm.js
+++ app/javascript/components/MountForm.js
@@ -2,14 +2,33 @@ import React from 'react'
 import useForm from 'react-hook-form'
 
 export default function MountForm(params) {
-  const {onSubmit} = params;
+  const { setMounts, mounts, csrf } = params;
   const { register, handleSubmit, watch, errors } = useForm()
 
   console.log(`name is ${watch('name')}`) // watch input value by passing the name of it
   console.log(`height is ${watch('height')}`) // watch input value by passing the name of it
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit((data) => {
+	  let new_mounts = mounts;
+	  let response = "";
+	  console.log(data);
+	  fetch('/mounts.json', {
+		  method: 'POST',
+		  headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-Token': csrf
+                  },		  
+		  body: JSON.stringify(data),
+	  }).then(response => response.json())
+	    .then(new_mount => { 
+	  	console.log(`mount is ${new_mount}`);
+    		new_mounts.push(new_mount);  
+    		setMounts(new_mounts);
+    		console.log(new_mounts);
+ 	});
+    })
+    }>
     {/* register your input into the hook by invoking the "register" function */}
       <input name="name" defaultValue="" ref={register} />
       
